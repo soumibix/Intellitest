275899208c4db9806161a9dbb48fec0b42e687e8
soumibix <soumi.biswas@iemlabs.com>
2025-11-17 18:57:12 +0530

    Add logout functionality to Layout components for Admin, Faculty, and User

--- src/Layout/Faculty/FacultyLayout.jsx
+++ src/Layout/Faculty/FacultyLayout.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Sidebar } from "../../Components/common/Sidebar";
 import { TopBar } from "../../Components/common/TopBar";
 import { facultyPortalNavItems } from "../../Data/facultyData";
+import { useAuth } from "../../AppRouter";
 
 function FacultyLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   const [isLargeScreen, setIsLargeScreen] = useState(false);
+  const {logout} = useAuth();
 
   useEffect(() => {
     const checkScreenSize = () => {
@@ -28,6 +30,14 @@ function FacultyLayout({ children }) {
     }
   };
 
+  const handleFacultyLogout = () => {
+    logout();
+    // navigate("/faculty/signin");
+    // localStorage.clear();
+    // sessionStorage.clear();
+    // window.location.href = "/admin/login";
+  };
+
   const closeSidebar = () => {
     if (!isLargeScreen) setSidebarOpen(false);
   };
@@ -43,7 +53,7 @@ function FacultyLayout({ children }) {
       />
 
       <div className="flex-1 flex flex-col min-w-0">
-        <TopBar userName="Faculty" onMenuClick={handleMenuClick} />
+        <TopBar userName="Faculty" onMenuClick={handleMenuClick} onLogout={handleFacultyLogout}/>
         <main className="flex-1 overflow-auto">{children}</main>
       </div>
     </div>
