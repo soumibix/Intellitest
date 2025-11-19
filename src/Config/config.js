8925902aae815c19b882fb557db348793c8379f7
debugxyzee <officialdip2002@gmail.com>
2025-11-19 18:31:11 +0530

    feat: Implement user test API with fetching, starting, and submitting functionalities

--- src/Config/config.js
+++ src/Config/config.js
@@ -46,6 +46,25 @@ export const API_ENDPOINTS = {
   // GET /test/testData?status=upcoming
   // GET /test/testData?search=CSE
   // GET /test/testData?status=upcoming&search=CSE&page=2&limit=5
+
+  // STUDENT TEST APIs
+  USER_GET_ALL_TESTS: "student/test/alltest",
+  USER_GET_TEST_BY_ID: (id) => `student/test/alltest?id=${id}`,
+  USER_START_TEST: (id) => `student/test/start/${id}`,
+  USER_SUBMIT_TEST: (id) => `student/test/submit/${id}`,
+  USER_GET_TEST_REPORT: (id) => `student/test/report/${id}`,
+
+  // With query parameters (these will be constructed dynamically)
+  // GET /test/testData?page=1&limit=10
+  // GET /test/testData?id=691c29b0588288e4044b78af
+  // GET /test/testData?status=upcoming
+  // GET /test/testData?search=CSE
+  // GET /test/testData?status=upcoming&search=CSE&page=2&limit=5
+
+  // Student test queries:
+  // GET /student/test/alltest?page=1&limit=5&department=IT&semester=2nd Semester&search=m
+  // GET /student/test/alltest?status=ongoing
+  // GET /student/test/alltest?status=upcoming&department=CSE
 };
 
 export default {
